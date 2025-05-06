5b4b640467c5d2450454db50519446bf6894a46f
Neb <bgirard@quarkslab.com>
2025-05-06 11:22:47 +0200

    Added a Wirego version string (in addition to API version string). Switching to v2.1

--- wirego_bridge/version.h
+++ wirego_bridge/version.h
@@ -1,8 +1,14 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-// Note: must match the ones defined in ../wirego/version.go
+//Wirego ZMQ API version
+//Note: must match the ones defined in language packages (../wirego_remote/go/wirego/version.go and ../wirego_remote/python/wirego.py)
+#define WIREGO_API_VERSION_MAJOR 2
+#define WIREGO_API_VERSION_MINOR 0
+
+
+//Wirego plugin version
 #define WIREGO_VERSION_MAJOR 2
-#define WIREGO_VERSION_MINOR 0
+#define WIREGO_VERSION_MINOR 1
 
 #endif
